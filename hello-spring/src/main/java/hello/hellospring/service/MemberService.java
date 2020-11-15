39992c46a3feb9613ee3205ec8c648c0b54bea0c
seungyeopme <seungyeop.me.dev@gmail.com>
2020-11-15 16:56:19 +0900

    Spring

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,24 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+//@Service
 public class MemberService {
 
-    private final MemberRepository memberRepository;
+//    private final MemberRepository memberRepository;
+    private MemberRepository memberRepository;
 
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+//    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -30,7 +40,7 @@ public class MemberService {
                 });
     }
 
-    /*전체 회원 조*/
+    /*전체 회원 조회*/
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
