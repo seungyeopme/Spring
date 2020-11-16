3572b9c9876194e5f5b1ea991f0d2b402bba5998
seungyeopme <seungyeop.me.dev@gmail.com>
2020-11-16 15:46:07 +0900

    Spring

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,13 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 //@Service
+@Transactional
 public class MemberService {
 
 //    private final MemberRepository memberRepository;
@@ -26,6 +28,7 @@ public class MemberService {
     }
 
     /*회원 가입*/
+//    @Transactional
     public Long join(Member member) {
         //같은 이름이 있는 중복 회원X
         validateDuplicateMember(member); //중복 회원 검증
