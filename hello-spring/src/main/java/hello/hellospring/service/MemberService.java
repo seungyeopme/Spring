93ed3bba2b4bebeb344cc22cc19b38e81f772a7f
seungyeopme <seungyeop.me.dev@gmail.com>
2020-11-18 11:27:57 +0900

    Spring

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -30,10 +30,19 @@ public class MemberService {
     /*회원 가입*/
 //    @Transactional
     public Long join(Member member) {
-        //같은 이름이 있는 중복 회원X
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            //같은 이름이 있는 중복 회원X
+            validateDuplicateMember(member);    //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -45,7 +54,14 @@ public class MemberService {
 
     /*전체 회원 조회*/
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs +"ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
