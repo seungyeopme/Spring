a44613d9d74940aab8b5d14c2b294a42ff7d3d2a
seungyeopme <seungyeop.me.dev@gmail.com>
2020-11-21 10:09:20 +0900

    Spring

---
+++ jpashop/src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+}
