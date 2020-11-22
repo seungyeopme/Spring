4b2fea79f4d90084e5552e525978689907950ce7
seungyeopme <seungyeop.me.dev@gmail.com>
2020-11-22 23:19:14 +0900

    Spring

--- jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -2,7 +2,7 @@ package jpabook.jpashop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test
 @SpringBootTest
 class JpashopApplicationTests {
 
